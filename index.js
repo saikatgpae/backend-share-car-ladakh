71cb61783bf073ffa076caeed00acc74c07dbcc0
Saikat Chakraborty <saikatgpae@gmail.com>
2023-04-24 12:58:31 +0530

    Initial commit -m.

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import ip from 'ip';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(8000, () => console.log('API running on port 5001'));
+
+app.get('/', (req, res) => res.json('App is running 😎'))
+
+
+// app.get('/', function(req, res){
+//   res.send("Hello world!");
+// });
+
+// app.get('/sign-up', (req, res) => {
+//   res.send({ a: 'b' });
+// });
+// app.post('/sign-up', (req) => {
+//   console.log('Frontend data', req.body.inputs);
+// });
+
+// app.get('/create-account', (req, res) => {
+//   res.send({ e: 'f' });
+// });
+// app.post('/create-account', (req) => {
+//   console.log('Frontend data', req.body.inputs);
+// });
+
+// const server = app.listen(5001, () => {
+//   const { port, address } = server.address();
+//   console.log('App listening at http://%s:%s', address, port);
+// });
