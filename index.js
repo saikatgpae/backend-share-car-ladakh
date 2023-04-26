ed6aadea951594a1c3d1763c29787ff7b6358514
Saikat Chakraborty <saikatgpae@gmail.com>
2023-04-26 14:35:15 +0530

    Add basic routes.

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
+// import express from 'express';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import mysql from 'mysql';
+// // var mysql = require('mysql');
+
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "share_car_ladakh"
+// });
+
+// var data;
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM cars", function (err, result, fields) {
+//     if (err) throw err;
+//     data = result;
+//     // console.log(result);
+//   });
+// });
+
+// app.get('/', function(req, res){
+//   res.send('hello world!');
+// });
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import mysql from 'mysql';
-// var mysql = require('mysql');
+import ip from 'ip';
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "share_car_ladakh"
-});
-
-var data;
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM cars", function (err, result, fields) {
-    if (err) throw err;
-    data = result;
-    // console.log(result);
-  });
-});
-
 app.get('/', function(req, res){
-  res.send('hello world!');
+  res.send("Hello world!");
+});
+
+app.get('/sign-up', (req, res) => {
+  res.send({ a: 'b' });
+});
+app.post('/sign-up', (req) => {
+  console.log('Frontend data', req.body.inputs);
+});
+
+app.get('/create-account', (req, res) => {
+  res.send({ e: 'f' });
+});
+app.post('/create-account', (req) => {
+  console.log('Frontend data', req.body.inputs);
+});
+
+const server = app.listen(5001, () => {
+  const { port, address } = server.address();
+  console.log('App listening at http://%s:%s', address, port);
 });
