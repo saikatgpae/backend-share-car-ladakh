01c5f703400ab09a130f7a511ebb1516cfa85c79
Saikat Chakraborty <saikatgpae@gmail.com>
2023-04-26 14:19:58 +0530

    Add basic quarry.

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import ip from 'ip';
+var express = require("express");
+var mysql = require('mysql');
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "share_car_ladakh"
+});
 
-app.listen(8000, () => console.log('API running on port 8000'));
-
-app.get('/', (req, res) => res.json('App is running 😎'))
-
-
-// app.get('/', function(req, res){
-//   res.send("Hello world!");
-// });
-
-// app.get('/sign-up', (req, res) => {
-//   res.send({ a: 'b' });
-// });
-// app.post('/sign-up', (req) => {
-//   console.log('Frontend data', req.body.inputs);
-// });
-
-// app.get('/create-account', (req, res) => {
-//   res.send({ e: 'f' });
-// });
-// app.post('/create-account', (req) => {
-//   console.log('Frontend data', req.body.inputs);
-// });
-
-// const server = app.listen(5001, () => {
-//   const { port, address } = server.address();
-//   console.log('App listening at http://%s:%s', address, port);
-// });
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM cars", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
---
+++ mysql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE invoices (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), phone INT);
+
+current_date DATETIME);
+
+
+
+journey_date DATETIME, rate INT, seatCount INT, seats VARCHAR(255), tax INT, time INT, total INT);
+
