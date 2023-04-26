3af6698d31936b38f217b29da455ea71458a60f9
Saikat Chakraborty <saikatgpae@gmail.com>
2023-04-26 14:44:46 +0530

    Remove error.

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mysql from 'mysql';
-// var mysql = require('mysql');
+// import express from 'express';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import mysql from 'mysql';
+// // var mysql = require('mysql');
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "share_car_ladakh"
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "share_car_ladakh"
+// });
 
-// var data;
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM cars", function (err, result, fields) {
-    if (err) throw err;
-    // data = result;
-    console.log(result);
-  });
-});
+// // var data;
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM cars", function (err, result, fields) {
+//     if (err) throw err;
+//     // data = result;
+//     console.log(result);
+//   });
+// });
 
-app.get('/', function(req, res){
-  res.send('hello world!');
-});
+// app.get('/', function(req, res){
+//   res.send('hello world!');
+// });
 
 
 
@@ -63,3 +63,17 @@ app.get('/', function(req, res){
 //   const { port, address } = server.address();
 //   console.log('App listening at http://%s:%s', address, port);
 // });
+
+
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import ip from 'ip';
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(8000, () => console.log('API running on port 5001'));
+app.listen(8000, () => console.log('API running on port 8000'));
+
+app.get('/', (req, res) => res.json('App is running 😎'));
