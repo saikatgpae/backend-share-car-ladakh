931957f58b7cc2819c0bad5aa11e296b082169f4
Saikat Chakraborty <saikatgpae@gmail.com>
2023-04-26 14:55:01 +0530

    Remove error.

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-// import express from 'express';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
-// import mysql from 'mysql';
-// // var mysql = require('mysql');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mysql from 'mysql';
+// var mysql = require('mysql');
 
-// const app = express();
-// app.use(cors());
-// app.use(bodyParser.json());
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "password",
-//   database: "share_car_ladakh"
-// });
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "share_car_ladakh"
+});
 
-// // var data;
-// con.connect(function(err) {
-//   if (err) throw err;
-//   con.query("SELECT * FROM cars", function (err, result, fields) {
-//     if (err) throw err;
-//     // data = result;
-//     console.log(result);
-//   });
-// });
-
-// app.get('/', function(req, res){
-//   res.send('hello world!');
-// });
+var data;
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM cars", function (err, result, fields) {
+    if (err) throw err;
+    data = result;
+    console.log(result);
+  });
+});
 
 
 
@@ -36,43 +32,10 @@
 // import cors from 'cors';
 // import bodyParser from 'body-parser';
 // import ip from 'ip';
-
 // const app = express();
 // app.use(cors());
 // app.use(bodyParser.json());
 
-// app.get('/', function(req, res){
-//   res.send("Hello world!");
-// });
-
-// app.get('/sign-up', (req, res) => {
-//   res.send({ a: 'b' });
-// });
-// app.post('/sign-up', (req) => {
-//   console.log('Frontend data', req.body.inputs);
-// });
-
-// app.get('/create-account', (req, res) => {
-//   res.send({ e: 'f' });
-// });
-// app.post('/create-account', (req) => {
-//   console.log('Frontend data', req.body.inputs);
-// });
-
-// const server = app.listen(5001, () => {
-//   const { port, address } = server.address();
-//   console.log('App listening at http://%s:%s', address, port);
-// });
-
-
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import ip from 'ip';
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
 app.listen(8000, () => console.log('API running on port 8000'));
 
-app.get('/', (req, res) => res.json('App is running 😎'));
+app.get('/', (req, res) => res.json(data));
