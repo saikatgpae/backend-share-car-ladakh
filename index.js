2d687b6f48a99da235a9c84d1cab074ce46d5af4
Saikat Chakraborty <saikatgpae@gmail.com>
2023-04-26 14:27:43 +0530

    Add routes.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var express = require("express");
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 var mysql = require('mysql');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -8,10 +14,16 @@ var con = mysql.createConnection({
   database: "share_car_ladakh"
 });
 
+var data;
 con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT * FROM cars", function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
+    data = result;
+    // console.log(result);
   });
+});
+
+app.get('/', function(req, res){
+  res.send(data);
 });
